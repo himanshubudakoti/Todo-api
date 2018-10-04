1dd9c7e99aa5748c908f465ffcf7df1109621b8e
Himanshu Budakoti <himanshubudakoti67@gmail.com>
2018-10-04 13:46:13 +0530

    Refractor API with underscore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3030;
@@ -20,15 +21,9 @@ app.get('/todos', function (req, res) {
 //GET todos by id {GET/todos/:id}
 app.get('/todos/:id', function (req, res) {
     var todoId = parseInt(req.params.id, 10);
-    var matchedTodo;
+    var matchedTodo = _.findWhere(todos, { id: todoId });
 
-    todos.forEach(function (todo) {
-        if (todoId === todo.id) {
-            matchedTodo = todo;
-        }
-    });
-
-    if (matchedTodo){
+    if (matchedTodo) {
         res.json(matchedTodo);
     } else {
         res.status(404).send();
@@ -36,8 +31,14 @@ app.get('/todos/:id', function (req, res) {
 });
 
 // POST /todos
-app.post('/todos', function (req, res){
-    var body = req.body;
+app.post('/todos', function (req, res) {
+    var body = _.pick(req.body, 'description', 'completed');
+
+    if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
+        return res.status(400).send();
+    }
+
+    body.description = body.description.trim();
 
     body.id = todoNextId++;
 
@@ -46,6 +47,6 @@ app.post('/todos', function (req, res){
     res.json(body);
 });
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log('Express listening on Port ' + PORT + '!');
 });
