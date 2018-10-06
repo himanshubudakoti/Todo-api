984366b64195b26cae2eed21ec8d350ca061e6b7
Himanshu Budakoti <himanshubudakoti67@gmail.com>
2018-10-06 13:23:28 +0530

    Convert GET /todos to sequelize

--- server.js
+++ server.js
@@ -16,26 +16,27 @@ app.get('/', function (req, res) {
 
 //GET all todos  {GET/todos?completed=true}
 app.get('/todos', function (req, res) {
-    var queryParams = req.query;
-    var filteredTodos = todos;
+    var query = req.query;
+    var where = {};
 
-    if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
-        filteredTodos = _.where(filteredTodos, {
-            completed: true
-        });
-    } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
-        filteredTodos = _.where(filteredTodos, {
-            completed: false
-        });
+    if (query.hasOwnProperty('completed') && query.completed === 'true') {
+        where.completed = true;
+    } else if (query.hasOwnProperty('completed') && query.completed == 'false') {
+        where.completed = false;
     }
 
-    if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
-        filteredTodos = _.filter(filteredTodos, function (todo) {
-            return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
-        });
+    if (query.hasOwnProperty('q') && query.q.length > 0) {
+        where.description = {
+            $like : '%' + query.q + '%'
+        };
     }
 
-    res.json(filteredTodos);
+    db.todo.findAll({where: where}).then(function (todos) {
+        res.json(todos);
+    }, function (e) {
+        res.status(500).send();
+    });
+    
 });
 
 //GET todos by id {GET/todos/:id}
@@ -51,15 +52,7 @@ app.get('/todos/:id', function (req, res) {
     }, function (e) {
         res.status(500).send();
     });
-    // var matchedTodo = _.findWhere(todos, {
-    //     id: todoId
-    // });
-
-    // if (matchedTodo) {
-    //     res.json(matchedTodo);
-    // } else {
-    //     res.status(404).send();
-    // }
+    
 });
 
 // POST /todos
@@ -71,17 +64,7 @@ app.post('/todos', function (req, res) {
     }, function (e) {
         res.status(400).json(e);
     });
-    // if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-    //     return res.status(400).send();
-    // }
-
-    // body.description = body.description.trim();
-
-    // body.id = todoNextId++;
-
-    // todos.push(body);
-
-    // res.json(body);
+    
 });
 
 //DELETE /todos/:id
